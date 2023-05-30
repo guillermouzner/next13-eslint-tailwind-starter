f4326a85b8253c52e448bc7cc8313b0c2dad1db5
guillermouzner <guillermouz16@gmail.com>
2023-05-30 18:43:41 -0300

    x

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,30 @@
-import "./globals.css";
+import {Inter} from "next/font/google";
 
-import {Inter} from "@next/font/google";
-const inter = Inter({subsets: ["latin"]});
+import "@/styles/globals.css";
+import {cn} from "@/lib/utils";
+import {Toaster} from "@/components/ui/toaster";
+import {TailwindIndicator} from "@/components/tailwind-indicator";
+import {ThemeProvider} from "@/components/theme-provider";
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+export default function RootLayout({children}: RootLayoutProps) {
   return (
-    <html className={inter.className} lang="es">
+    <html suppressHydrationWarning lang="es">
       <head />
-      <body>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
+        <ThemeProvider enableSystem attribute="class" defaultTheme="system">
+          {children}
+          <Toaster />
+          <TailwindIndicator />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
